dbdcb7e918d3be5c9308a0aaebf2b78e54badf1c
Luka T <l.tsiklauri@itmegainsight.com>
2024-10-25 17:14:49 +0400

    configs added

---
+++ src/Config/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'mongodb' => [
+        'driver' => 'mongodb',
+        'host' => 'localhost',
+        'port' => 27017,
+        'database' => 'epam_analytics',
+    ],
+];
---
+++ src/Controller/EventController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+final readonly class EventController
+{
+    public function __construct()
+    {
+
+    }
+
+    public function create(): void
+    {
+
+    }
+}
