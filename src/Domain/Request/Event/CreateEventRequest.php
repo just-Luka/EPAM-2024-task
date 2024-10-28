a550ceb9669dc1a42174e8131cfe33d846325a61
Luka T <l.tsiklauri@itmegainsight.com>
2024-10-28 23:18:08 +0400

    Validation rules added

--- src/Domain/Request/Event/CreateEventRequest.php
+++ src/Domain/Request/Event/CreateEventRequest.php
@@ -11,10 +11,10 @@ final class CreateEventRequest extends baseRequest
     public function rules(): array
     {
         return [
-            'id'         => [self::required(), self::isInt()],
-            'user_id'    => [self::required(), self::isInt()],
-            'event_type' => [self::required(), self::isString()],
-            'timestamp'  => [self::required(), self::isInt()],
+            'id'         => [self::required(), self::isInt(), self::minInt(0)],
+            'user_id'    => [self::required(), self::isInt(), self::minInt(0)],
+            'event_type' => [self::required(), self::isString(), self::isInOptions(['click', 'view', 'purchase'])],
+            'timestamp'  => [self::required(), self::isInt(), self::isTimestamp()],
             'metadata'   => [],
         ];
     }
--- src/Trait/Validatable.php
+++ src/Trait/Validatable.php
@@ -6,6 +6,9 @@ namespace App\Trait;
 
 trait Validatable
 {
+    /**
+     * @return array
+     */
     protected static function required(): array
     {
         return [
@@ -14,6 +17,9 @@ trait Validatable
         ];
     }
 
+    /**
+     * @return array
+     */
     protected static function isString(): array
     {
         return [
@@ -22,6 +28,9 @@ trait Validatable
         ];
     }
 
+    /**
+     * @return array
+     */
     protected static function isInt(): array
     {
         return [
@@ -30,11 +39,61 @@ trait Validatable
         ];
     }
 
+    /**
+     * @return array
+     */
     protected static function isArray(): array
     {
         return [
             'rule' => fn($value): bool => is_array($value),
             'message' => 'This field must be an array.',
+        ];
+    }
+
+    /**
+     * @param int $min
+     * @return array
+     */
+    protected static function minInt(int $min): array
+    {
+        return [
+            'rule' => fn($value): bool => is_int($value) && $value >= $min,
+            'message' => "This field must be an integer greater than or equal to {$min}.",
+        ];
+    }
+
+    /**
+     * @param int $max
+     * @return array
+     */
+    protected static function maxInt(int $max): array
+    {
+        return [
+            'rule' => fn($value): bool => is_int($value) && $value <= $max,
+            'message' => "This field must be an integer less than or equal to {$max}.",
+        ];
+    }
+
+    /**
+     * @param array $options
+     * @return array
+     */
+    protected static function isInOptions(array $options): array
+    {
+        return [
+            'rule' => fn($value): bool => is_string($value) && in_array($value, $options, true),
+            'message' => 'This field must be one of the following: ' . implode(', ', $options) . '.',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected static function isTimestamp(): array
+    {
+        return [
+            'rule' => fn($value): bool => is_int($value) && $value > 0 && $value <= PHP_INT_MAX,
+            'message' => 'This field must be a valid timestamp.',
         ];
     }
 }
